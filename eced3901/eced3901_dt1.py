08f345113b9442c5e7c9c9ffbc719d6de68496e6
Natalie290 <nt288290@dal.ca>
2025-01-22 23:48:38 -0400

    Update eced3901_dt1.py
    Lidar box detection with loop

--- eced3901/eced3901_dt1.py
+++ eced3901/eced3901_dt1.py
@@ -161,7 +161,9 @@ class NavigateSquare(Node):
 
         # WARNING: Check for updates, note this is set and will run backwards
         #          on the physical model but correctly in simulation.
-        self.x_vel = -0.2
+        self.x_vel = -0.2 # forward speed
+	self.turn_speed = 1.0 # angular speed for turns
+	self.loops = 0
 
         self.x_now = 0.0
         self.x_init = 0.0
@@ -171,6 +173,8 @@ class NavigateSquare(Node):
         self.d_aim = 1.0
 
         self.laser_range = None
+	self.before_box = 0.0
+
 
         # Subscribe to odometry
         self.sub_odom = self.create_subscription(
@@ -237,27 +241,41 @@ class NavigateSquare(Node):
 
     # Square navigation states: forward motion and turning
     if self.state == "before_box":
-        if laser_ranges_min and laser_ranges_min > 0.5:
-            # Continue moving forward
+        if laser_ranges_min and laser_ranges_min > 0.3:
+            # Continue moving forward till box detected
             msg.linear.x = self.x_vel
             msg.angular.z = 0.0
         else:
             # Obstacle detected, start turning
-            self.state = "turn"
-            self.turn_start_time = self.get_clock().now()
-    elif self.state == "along_edge":
-        # Turn in place for a set duration (adjust to complete a ~90° turn)
-        if (self.get_clock().now() - self.turn_start_time).nanoseconds / 1e9 < 2.0:
-            msg.linear.x = 0.0
-            msg.angular.z = 1.0
+            self.state = "along_box"
+    elif self.state == "along_box":
+        # move along egde of box till at corner
+        if laser_ranges_min and laser_ranges_min > 0.05:
+            msg.linear.x = self.x_vel
+            msg.angular.z = 0.0
         else:
             # Finished turning, switch back to forward motion
-            self.state = "forward"
-            self.x_init = self.x_now
-            self.y_init = self.y_now
+            self.state = "corner"
+            self.turn_start_time = self.get_clock().now()
     elif self.state == "corner":
-	    
-
+	if (self.get_clock().now() - self.turn_start_time).nanoseconds / 1e9 < 2.0:
+            # turn 90 left
+            msg.linear.x = 0.0
+            msg.angular.z = self.turn_speed
+        else:
+            # Obstacle detected, start turning
+            self.loops += 1
+	
+	    if self.loops >= 4:
+		msg.linear.x = 0.0
+		msg.angular.z = 0.0
+		self.state = "stopped"
+	    else:
+		self.state = "before_box"
+    elif self.state == "stopped":
+	    msg.linear.x = 0.0
+	    msg.angular.z = 0.0
+				
     # Publish the velocity command
     self.pub_vel.publish(msg)
     self.get_logger().info("Sent: " + str(msg))
@@ -268,8 +286,8 @@ class NavigateSquare(Node):
 
         #self.get_logger().info(f'Timer hit')
 
-        self.control_example_odom()
-        #self.control_example_lidar()  
+        #self.control_example_odom()
+        self.control_example_lidar()  
 
     def odom_callback(self, msg):
         """Callback on 'odom' subscription"""
