201aeea2de0c33ec1bfa3e1128bb8c346f3fb63d
Natalie290 <nt288290@dal.ca>
2025-03-14 18:18:47 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,40 +1,39 @@
-from setuptools import setup
-import os
-from glob import glob
+from machine import Pin, PWM
+from time import sleep
 
-package_name = 'eced3901'
+# Define servo pins
+servo1 = PWM(Pin(17))
+servo2 = PWM(Pin(18))
 
-setup(
-    name=package_name,
-    version='0.0.0',
-    packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-        (os.path.join('share', package_name, 'maps'), glob('maps/*')),
-        (os.path.join('share', package_name, 'models'), glob('models/*.urdf')),
-        (os.path.join('share', package_name, 'models', 'eced3901bot_description'), glob('models/eced3901bot_description/*')),
-        (os.path.join('share', package_name, 'models', 'eced3901_lab4'), glob('models/eced3901_lab4/*')),        
-        (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
-        (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
-        (os.path.join('share', package_name, 'config'), glob('config/*')),
-        (os.path.join('share', package_name, 'params'), glob('params/*')),
-    ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='student',
-    maintainer_email='student@dal.ca',
-    description='ECED3901 package for all things robot',
-    license='GNU GPLv3',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-            'talker = eced3901.publisher_member_function:main',
-            'dt1 = eced3901.eced3901_dt1:main',
-            'serialsensor = eced3901.eced3901_serialcode:main',
-        ],
-    },
-)
+# Set PWM frequency to 50 Hz (standard for servos)
+servo1.freq(80)
+servo2.freq(80)
 
+def set_angle(servo, angle):
+    # Convert angle (0 to 180) to duty cycle (1000 to 9000)
+    duty = int((angle / 180) * 8000 + 1000)
+    servo.duty_u16(duty)
+
+def move_servo(servo, start, end, delay_time):
+    step = 1 if start < end else -1
+    for pos in range(start, end + step, step):
+        set_angle(servo, pos)
+        sleep(delay_time / 1000)
+
+#while True:
+
+    # Move first servo from 0 to 180 degrees and back
+move_servo(servo1, 90, 180, 15)
+move_servo(servo1, 0, 90, 15)
+move_servo(servo1, 90, 180, 15)
+move_servo(servo1, 0, 90, 15)
+move_servo(servo1, 90, 180, 15)
+move_servo(servo1, 0, 90, 15)
+move_servo(servo1, 90, 180, 15)
+move_servo(servo1, 0, 90, 15)
+move_servo(servo1, 90, 180, 15)
+move_servo(servo1, 0, 90, 15)
+move_servo(servo1, 90, 180, 15)
+    # Move second servo from 0 to 180 degrees and back
+    #move_servo(servo2, 15, 65, 15)
+    #move_servo(servo2,45, 0, 15)
